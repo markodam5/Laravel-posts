b5d385d615383bb3a55a7dcabdc7a334ffe836ee
Marko Damjanac <36675797+markodam5@users.noreply.github.com>
2026-04-22 10:23:39 +0200

    Add files via upload

---
+++ laravel_posts.sql
@@ -0,0 +1,59 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.32-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.7.0.6850
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping structure for table laravel_test.posts
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) DEFAULT NULL,
+  `body` text DEFAULT NULL,
+  `user_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table laravel_test.posts: ~7 rows (approximately)
+INSERT INTO `posts` (`id`, `title`, `body`, `user_id`) VALUES
+	(1, 'First post title updated', 'First post description updated.', 1),
+	(5, 'Second post title update', 'Second post description update', 1),
+	(6, 'Third Post Title', 'Third post Description', 2),
+	(7, 'Fourth Post Title', 'Fourth Post Description', 1),
+	(8, 'Fifth Post Title', 'Fifth Post Title', 2),
+	(9, 'Sixth Post Description', 'Sixth Post Title', 2),
+	(12, 'Seventh post title', 'Seventh post description', NULL);
+
+-- Dumping structure for table laravel_test.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `user_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table laravel_test.users: ~2 rows (approximately)
+INSERT INTO `users` (`id`, `name`, `phone`, `email`, `user_id`) VALUES
+	(1, 'Jennifer', '1234567890', 'jennifer@gmail.com', NULL),
+	(2, 'John', '22334455', 'john@gmail.com', NULL);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
